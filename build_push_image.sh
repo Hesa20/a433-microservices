58b86ca33015941b0339d4fc1bda5c5458fceffc
Hesa Firdaus <109150746+Hesa20@users.noreply.github.com>
2024-07-27 23:42:25 +0700

    update script docker compose & build push image

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,14 +1,14 @@
-# # build docker image
+# build docker image
 docker build -t item-app:v1 .
+
+# show all docker images
+docker image ls
+
+# tag docker image  
+docker tag item-app:v1 hesa21/item-app:v1
 
 # login to docker hub
 docker login -u hesa21 -p $(echo $PASSWORD_DOCKER_HUB)
 
 # push docker image to docker hub
-docker tag item-app:v1 hesa21/item-app:v1
 docker push hesa21/item-app:v1
-
-# show all docker images
-docker image ls
-
-
